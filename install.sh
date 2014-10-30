e2d0d267e585420a2555e9d6cc27ac1ec714edb6
Marc Bradshaw <marc@marcbradshaw.net>
2014-10-30 16:00:19 +1100

    Updated install script

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cp init /etc/init.d/authentication_milter
+cp Mail-Milter-Authentication/share/authentication_milter.init /etc/init.d/authentication_milter
 #cp mail-dmarc.ini /etc/
-cp authentication_milter.json /etc/
+#cp authentication_milter.json /etc/
 
 cd Mail-Milter-Authentication
 perl Makefile.PL
