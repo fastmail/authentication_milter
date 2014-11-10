140a4b0c14db30efc6e6e85ad532ee2176be0be9
Marc Bradshaw <marc@marcbradshaw.net>
2014-11-10 14:20:11 +1100

    prepare for restructure

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-cp Mail-Milter-Authentication/share/authentication_milter.init /etc/init.d/authentication_milter
+cp share/authentication_milter.init /etc/init.d/authentication_milter
 #cp mail-dmarc.ini /etc/
 #cp authentication_milter.json /etc/
 
-cd Mail-Milter-Authentication
 perl Makefile.PL
 make
 make install
