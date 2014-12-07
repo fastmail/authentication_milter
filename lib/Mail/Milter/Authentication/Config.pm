ff443a2a09350e13053c914a0e928bc87bda11c8
Marc Bradshaw <marc@marcbradshaw.net>
2014-12-07 19:17:41 +1100

    Allow config to be split into per handler files

--- lib/Mail/Milter/Authentication/Config.pm
+++ lib/Mail/Milter/Authentication/Config.pm
@@ -12,6 +12,29 @@ our @EXPORT_OK = qw{
 
 use JSON;
 
+sub load_file {
+    my ( $file ) = @_;
+
+    if ( !-e $file ) {
+        die "Could not find configuration file $file";
+    }
+
+    my $text;
+    {
+        open my $cf, '<',
+          $file || die "Could not open configuration file $file";
+        my @t = <$cf>;
+        close $cf;
+        $text = join( q{}, @t );
+    }
+
+    my $json = JSON->new();
+    my $data = $json->decode($text)
+      || die "Error parsing config file $file";
+
+    return $data;
+}
+
 {
     my $CONFIG;
 
@@ -20,22 +43,25 @@ use JSON;
         return $CONFIG if $CONFIG;
 
         my $file = '/etc/authentication_milter.json';
-        if ( !-e $file ) {
-            die "Could not find configuration file $file";
-        }
 
-        my $text;
-        {
-            open my $cf, '<',
-              $file || die "Could not open configuration file $file";
-            my @t = <$cf>;
-            close $cf;
-            $text = join( q{}, @t );
-        }
+        $CONFIG = load_file( $file );
 
-        my $json   = JSON->new();
-        $CONFIG = $json->decode($text)
-          || die "Error parsing config file $file";
+        my $folder = '/etc/authentication_milter.d';
+        if ( -d $folder ) {
+            my $dh;
+            opendir $dh, $folder;
+            my @config_files =
+                sort
+                grep { $_ =~ /\.json/ }
+                grep { not $_ =~ /^\./ }
+                readdir($dh);
+            closedir $dh;
+            foreach my $file ( @config_files ) {
+                $file =~ /(^.*)\.json$/;
+                my $handler = $1;
+                $CONFIG->{'handlers'}->{$handler} = load_file( join( '/', $folder, $file ) );
+            }
+        }
 
         my @standard_handlers = qw{ Core };
         my @load_handlers = keys %{ $CONFIG->{'handlers'} };
