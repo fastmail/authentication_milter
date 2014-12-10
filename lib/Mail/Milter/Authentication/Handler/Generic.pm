4d69c9b9509a2a5aef11a7f1f002b6aeeab05237
Marc Bradshaw <marc@marcbradshaw.net>
2014-12-11 09:34:30 +1100

    Merged Protocol and Handler::Generic

--- lib/Mail/Milter/Authentication/Handler/Generic.pm
+++ lib/Mail/Milter/Authentication/Handler/Generic.pm
@@ -7,13 +7,14 @@ use English;
 
 our $VERSION = 0.5;
 
-use base 'Mail::Milter::Authentication::Protocol';
-
 use Email::Address;
 use English;
 use Module::Load;
 use Sys::Syslog qw{:standard :macros};
 use Sys::Hostname;
+
+use Mail::Milter::Authentication::Config qw{ get_config };
+use Mail::Milter::Authentication::Constants qw { :all };
 
 sub new {
     my ( $class, $wire ) = @_;
@@ -564,5 +565,60 @@ sub append_header {
         'value' => $value,
       };
 }
+
+
+
+sub smfis_continue {
+    return SMFIS_CONTINUE;
+}
+
+sub smfis_tempfail {
+    return SMFIS_TEMPFAIL;
+}
+
+sub smfis_reject {
+    return SMFIS_REJECT;
+}
+
+sub smfis_discard {
+    return SMFIS_DISCARD;
+}
+
+sub smfis_accept {
+    return SMFIS_ACCEPT;
+}
+
+
+
+sub write_packet {
+    my ( $self, $type, $data ) = @_;
+    my $wire = $self->{'wire'};
+    $wire->write_packet( $type, $data );
+}
+
+sub add_header {
+    my ( $self, $key, $value ) = @_;
+    my $wire = $self->{'wire'};
+    my $CONFIG = get_config();
+    return if $CONFIG->{'dryrun'};
+    $wire->add_header( $key, $value );
+}
+
+sub insert_header {
+    my ( $self, $index, $key, $value ) = @_;
+    my $wire = $self->{'wire'};
+    my $CONFIG = get_config();
+    return if $CONFIG->{'dryrun'};
+    $wire->insert_header( $index, $key, $value );
+}
+
+sub change_header {
+    my ( $self, $key, $index, $value ) = @_;
+    my $wire = $self->{'wire'};
+    my $CONFIG = get_config();
+    return if $CONFIG->{'dryrun'};
+    $wire->change_header( $key, $index, $value );
+}
+
 
 1;
