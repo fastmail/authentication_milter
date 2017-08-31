947f99ba15bf0722e16071e7c5da53a671f80df4
Marc Bradshaw <marc@marcbradshaw.net>
2017-08-31 11:50:59 +1000

    Make IPRev handler rfc complient wrt multiple PTR records for an address

--- lib/Mail/Milter/Authentication/Handler/IPRev.pm
+++ lib/Mail/Milter/Authentication/Handler/IPRev.pm
@@ -7,6 +7,7 @@ use version; our $VERSION = version->declare('v1.1.3');
 use Net::DNS;
 use Net::IP;
 use Sys::Syslog qw{:standard :macros};
+use List::Util qw{ uniq };
 
 sub default_config {
     return {};
@@ -45,100 +46,145 @@ sub connect_callback {
     my $ip_address = $self->ip_address();
     my $i1         = $ip;
     my $resolver = $self->get_object('resolver');
-    my $domain;
-    my $result;
-    my $detail;
 
-    # We do not consider multiple PTR records,
-    # as this is not a recomended setup
+    my $lookup_limit = 10;
+    # Make this a config item
+
+    my $ptr_list = {};
+    my @error_list;
+
     my $packet = $resolver->query( $ip_address, 'PTR' );
+    $lookup_limit--;
     if ($packet) {
         foreach my $rr ( $packet->answer ) {
             next unless $rr->type eq "PTR";
-            $domain = $rr->rdatastr;
+            $ptr_list->{ $rr->rdatastr } = [];
         }
+    }
+
+    if ( $resolver->errorstring() ) {
+        $self->_dns_error( 'PTR', $ip_address, $resolver->errorstring );
+        push @error_list, 'Error ' . $resolver->errorstring() . " looking up $ip_address PTR";
+    }
+
+    if ( ! keys %$ptr_list ) {
+        push @error_list, "NOT FOUND";
+    }
+
+    DOMAINLOOKUP:
+    foreach my $domain ( uniq sort keys %$ptr_list ) {
+
+        my $ip_list = [];
+
+        if ( $ip_address =~ /:/ ) {
+            # We are living in the future!
+
+            my $errors6;
+            my $errors4;
+            ( $lookup_limit, $ip_list, $errors6 ) = $self->_address_for_domain( 'AAAA', $domain, $lookup_limit );
+            if ( ! @$ip_list ) {
+                # We got nothing, try ip4
+                ( $lookup_limit, $ip_list, $errors4 ) = $self->_address_for_domain( 'A', $domain, $lookup_limit );
+            }
+            if ( ! @$ip_list ) {
+                foreach my $error ( @$errors4 ) {
+                    push @error_list, "Error $error looking up $domain A";
+                }
+                foreach my $error ( @$errors6 ) {
+                    push @error_list, "Error $error looking up $domain AAAA";
+                }
+            }
+
+        }
+        else {
+
+            my $errors6;
+            my $errors4;
+            ( $lookup_limit, $ip_list, $errors4 ) = $self->_address_for_domain( 'A', $domain, $lookup_limit );
+            if ( ! @$ip_list ) {
+                # We got nothing, try ip6
+                ( $lookup_limit, $ip_list, $errors6 ) = $self->_address_for_domain( 'AAAA', $domain, $lookup_limit );
+            }
+            if ( ! @$ip_list ) {
+                foreach my $error ( @$errors4 ) {
+                    push @error_list, "Error $error looking up $domain A";
+                }
+                foreach my $error ( @$errors6 ) {
+                    push @error_list, "Error $error looking up $domain AAAA";
+                }
+            }
+
+        }
+
+        $ptr_list->{ $domain } = $ip_list;
+
+    }
+
+    my @match_list;
+    foreach my $domain ( uniq sort keys %$ptr_list ) {
+        foreach my $address ( uniq sort @{ $ptr_list->{ $domain } } ) {
+            my $i2 = Net::IP->new($address);
+            my $is_overlap = $i1->overlaps($i2) || 0;
+            if ( $is_overlap == $IP_IDENTICAL ) {
+                $domain =~ s/\.$//;
+                push @match_list, $domain;
+            }
+        }
+    }
+
+    if ( ! @match_list ) {
+        # Failed to match IP against looked up domains
+        my $comment = join( ',', @error_list );
+        $self->dbgout( 'IPRevCheck', "fail - $comment", LOG_DEBUG );
+        my $header =
+            $self->format_header_entry( 'iprev',        'fail' ) . ' '
+          . $self->format_header_entry( 'policy.iprev', $ip_address ) . ' ' . '('
+          . $self->format_header_comment($comment) . ')';
+        $self->add_c_auth_header($header);
     }
     else {
-        $self->_dns_error( 'PTR', $ip_address, $resolver->errorstring );
+        # We have a pass
+        my $comment = join( ',', @match_list );
+        $self->{'verified_ptr'} = $comment;
+        $self->dbgout( 'IPRevCheck', "pass - $comment", LOG_DEBUG );
+        my $header =
+            $self->format_header_entry( 'iprev',        'pass' ) . ' '
+          . $self->format_header_entry( 'policy.iprev', $ip_address ) . ' ' . '('
+          . $self->format_header_comment($comment) . ')';
+        $self->add_c_auth_header($header);
     }
-
-    my $a_error = q{};
-    if ($domain) {
-        my $packet = $resolver->query( $domain, 'A' );
-        if ($packet) {
-          APACKET:
-            foreach my $rr ( $packet->answer ) {
-                next unless $rr->type eq "A";
-                my $address    = $rr->rdatastr;
-                my $i2         = Net::IP->new($address);
-                my $is_overlap = $i1->overlaps($i2) || 0;
-                if ( $is_overlap == $IP_IDENTICAL ) {
-                    $result = 'pass';
-                    last APACKET;
-                }
-            }
-        }
-        else {
-            # Don't log this right now, might be an AAAA only host.
-            $a_error = $resolver->errorstring;
-        }
-
-
-        if ( $a_error eq 'NXDOMAIN' ) {
-            $self->_dns_error( 'A', $domain, $a_error );
-            $detail = 'NXDOMAIN';
-        }
-        else {
-            if ( !$result ) {
-                my $packet = $resolver->query( $domain, 'AAAA' );
-                if ($packet) {
-                  APACKET:
-                    foreach my $rr ( $packet->answer ) {
-                        next unless $rr->type eq "AAAA";
-                        my $address    = $rr->rdatastr;
-                        my $i2         = Net::IP->new($address);
-                        my $is_overlap = $i1->overlaps($i2) || 0;
-                        if ( $is_overlap == $IP_IDENTICAL ) {
-                            $result = 'pass';
-                            last APACKET;
-                        }
-                    }
-                }
-                else {
-                    # Log A errors now, as they become relevant if AAAA also fails.
-                    $self->_dns_error( 'A', $domain, $a_error ) if $a_error;
-                    $self->_dns_error( 'AAAA', $domain, $resolver->errorstring );
-                }
-            }
-        }
-    }
-
-    if ( !$result ) {
-        $result = 'fail';
-    }
-
-    if ( !$domain ) {
-        $result = 'fail';
-        $domain = 'NOT FOUND';
-    }
-
-    $domain =~ s/\.$//;
-
-    if ( $result eq 'pass' ) {
-        $self->{'verified_ptr'} = $domain;
-    }
-
-    my $comment = $domain;
-    $comment .= " $detail" if $detail;
-
-    $self->dbgout( 'IPRevCheck', $result, LOG_DEBUG );
-    my $header =
-        $self->format_header_entry( 'iprev',        $result ) . ' '
-      . $self->format_header_entry( 'policy.iprev', $ip_address ) . ' ' . '('
-      . $self->format_header_comment($comment) . ')';
-    $self->add_c_auth_header($header);
 
     return;
+}
+
+sub _address_for_domain {
+    my ( $self, $type, $domain, $lookup_limit ) = @_;
+
+    my @fwd_errors;
+    my @ip_list;
+
+    my $resolver = $self->get_object('resolver');
+
+    $lookup_limit--;
+    if ( $lookup_limit <= 0 ) {
+        return ( 0, \@ip_list, [ 'Lookup limit reached' ] );
+    }
+    my $packet = $resolver->query( $domain, $type );
+
+    if ($packet) {
+        foreach my $rr ( $packet->answer ) {
+            next unless $rr->type eq $type;
+            my $address = $rr->rdatastr;
+            push @ip_list, $address;
+        }
+    }
+
+    if ( $resolver->errorstring() ) {
+        $self->_dns_error( $type, $domain, $resolver->errorstring );
+        push @fwd_errors, 'Error ' . $resolver->errorstring() . " looking up $domain $type";
+    }
+
+    return ( $lookup_limit, \@ip_list, \@fwd_errors );
 }
 
 sub close_callback {
