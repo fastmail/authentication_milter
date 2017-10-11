aadf5f0c867e4ab049dbcfaf1616e907090ff46c
Marc Bradshaw <marc@marcbradshaw.net>
2017-10-11 19:28:25 +1100

    Added the TestTimeout handler to git

---
+++ t/Mail/Milter/Authentication/Handler/TestTimeout.pm
@@ -0,0 +1,29 @@
+package Mail::Milter::Authentication::Handler::TestTimeout;
+
+use strict;
+use warnings;
+use base 'Mail::Milter::Authentication::Handler';
+use version; our $VERSION = version->declare('v1.1.4');
+
+use Data::Dumper;
+use English qw{ -no_match_vars };
+use Sys::Syslog qw{:standard :macros};
+
+sub _timeout {
+    alarm ( 1 );
+    sleep 10;
+}
+
+
+sub connect_callback { _timeout(); }
+sub helo_callback {  _timeout(); }
+sub envfrom_callback { _timeout(); }
+sub envrcpt_callback { _timeout(); }
+sub header_callback { _timeout(); }
+sub eoh_callback { _timeout(); }
+sub body_callback { _timeout(); }
+sub eom_callback { _timeout(); }
+sub abort_callback { _timeout(); }
+sub close_callback { _timeout(); }
+
+1;
