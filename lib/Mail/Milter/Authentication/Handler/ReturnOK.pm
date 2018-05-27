614fdd3654d92f5a061cb21976450bcac39c5ac2
Marc Bradshaw <marc@marcbradshaw.net>
2018-05-27 17:14:40 +1000

    Rewrite the ReturnOK Handler

--- lib/Mail/Milter/Authentication/Handler/ReturnOK.pm
+++ lib/Mail/Milter/Authentication/Handler/ReturnOK.pm
@@ -38,167 +38,225 @@ sub _check_address {
 
     my $domain = lc $self->get_domain_from( $email );
 
-    $self->_check_domain ( $domain, $type, 0 );
-
-    # Get Org domain and check that if different.
-    if ( $self->is_handler_loaded( 'DMARC' ) ) {
-        my $dmarc_handler = $self->get_handler('DMARC');
-        my $dmarc_object = $dmarc_handler->get_dmarc_object();
-        if ( $domain ) {
-            my $org_domain = eval{ $dmarc_object->get_organizational_domain( $domain ); };
-            $self->handle_exception( $@ );
-            if ( $org_domain eq $domain ) {
-                $self->{ 'metrics' }->{ $type . '_is_org_domain' } = 'yes';
-                push @{ $self->{ 'details' } }, Mail::AuthenticationResults::Header::SubEntry->new()->set_key( $type . '_is_org_domain' )->safe_set_value( 'yes' );
-            }
-            else {
-                $self->_check_domain( $org_domain, $type, 1 );
-                $self->{ 'metrics' }->{ $type . '_is_org_domain' } = 'no';
-                push @{ $self->{ 'details' } }, Mail::AuthenticationResults::Header::SubEntry->new()->set_key( $type . '_is_org_domain' )->safe_set_value( 'no' );
-            }
-        }
-    }
+    $self->_check_domain ( $domain, $type );
 
     return;
 }
 
-sub _check_domain {
-    my ( $self, $domain, $type, $is_org ) = @_;
-
+sub _check_domain_rr {
+    my ( $self, $domain, $rrtype ) = @_;
     my $resolver = $self->get_object('resolver');
-
-    if ( ! $domain ) {
-        $self->log_error( "ReturnOK: No Domain for $type" );
-        $self->{ 'metrics' }->{ ( $is_org ? 'org_' : '' ) . $type } = 'empty';
-        return;
-    }
-
-    my $result = 'fail';
-    $self->{ 'metrics' }->{ ( $is_org ? 'org_' : '' ) . $type } = 'fail';
-    my @details;
-
-    push @details, Mail::AuthenticationResults::Header::SubEntry->new()->set_key( 'domain' )->safe_set_value( $domain );
-
-    my $has_mx   = 0;
-    my $has_a    = 0;
-    my $has_aaaa = 0;
-    my $packet;
-
+    my $return = {
+        'result' => 0,
+        'error'  => '',
+    };
     eval {
-        $packet = $resolver->query( $domain, 'MX' );
+        my $packet = $resolver->query( $domain, $rrtype );
         if ($packet) {
             foreach my $rr ( $packet->answer ) {
-                next unless $rr->type eq "MX";
-                $has_mx = 1;
-                $result = 'pass';
-                $self->{ 'metrics' }->{ 'result' } = 'pass' if ! $is_org;
-                $self->{ 'metrics' }->{ ( $is_org ? 'org_' : '' ) . $type } = 'mx_pass';
+                next unless $rr->type eq $rrtype;
+                $return->{ 'result' } = 1;
+                ## TODO Check the returned record is in fact valid for its type
                 last;
             }
         }
         else {
             my $error = $resolver->errorstring;
             if ( $error ) {
-                push @details, Mail::AuthenticationResults::Header::SubEntry->new()->set_key( 'mx.error' )->safe_set_value( $error );
-            }
-            else {
-                push @details, Mail::AuthenticationResults::Header::SubEntry->new()->set_key( 'mx.error' )->safe_set_value( 'none' );
+                $return->{ 'error' } = $error;
             }
         }
     };
     if ( my $error = $@ ) {
         $self->handle_exception( $error );
-        $self->log_error( "ReturnOK: Domain lookup fatal error $error for $domain" );
-        push @details, Mail::AuthenticationResults::Header::SubEntry->new()->set_key( 'mx.error' )->safe_set_value( 'lookup_error' );
+        $return->{ 'error' } = 'lookup_error';
+        $self->log_error( "ReturnOK: Domain lookup fatal error $error for $domain $rrtype" );
+    }
+    return $return;
+}
+
+sub _check_domain {
+    my ( $self, $domain, $type ) = @_;
+
+    my $metrics = {};
+    my @details;
+
+    if ( ! $domain ) {
+        $self->log_error( "ReturnOK: No Domain for $type" );
+        my $header = Mail::AuthenticationResults::Header::Entry->new()->set_key( 'x-return-mx' )->safe_set_value( 'none' );
+        $metrics->{ $type . '_result' } = 'none';
+        $self->dbgout( 'ReturnOKCheck', 'none', LOG_DEBUG );
+        $self->add_auth_header($header);
+        push @{ $self->{ 'metrics' } }, $metrics;
+        return;
     }
 
-    if ( ! $has_mx ) {
+    push @details, Mail::AuthenticationResults::Header::SubEntry->new()->set_key( $type . '.domain' )->safe_set_value( $domain );
 
-        eval {
-            $packet = $resolver->query( $domain, 'A' );
-            if ($packet) {
-                foreach my $rr ( $packet->answer ) {
-                    next unless $rr->type eq "A";
-                    $has_a = 1;
-                    $result = 'warn';
-                    $self->{ 'metrics' }->{ 'result' } = 'warn' if $self->{ 'metrics' }->{ 'result' } ne 'pass' and ( ! $is_org );
-                    last;
-                }
+    # Get Org domain and check that if different.
+    my $is_org = -1;
+    my $org_domain;
+    if ( $self->is_handler_loaded( 'DMARC' ) ) {
+        my $dmarc_handler = $self->get_handler('DMARC');
+        my $dmarc_object = $dmarc_handler->get_dmarc_object();
+        if ( $domain ) {
+            $org_domain = eval{ $dmarc_object->get_organizational_domain( $domain ); };
+            $self->handle_exception( $@ );
+            if ( $org_domain eq $domain ) {
+                $is_org = 1;
+                $metrics->{ $type . '_is_org_domain' } = 'yes';
             }
             else {
-                my $error = $resolver->errorstring;
-                if ( $error ) {
-                    push @details, Mail::AuthenticationResults::Header::SubEntry->new()->set_key( 'a.error' )->safe_set_value( $error );
-                }
-                else {
-                    push @details, Mail::AuthenticationResults::Header::SubEntry->new()->set_key( 'a.error' )->safe_set_value( 'none' );
-                }
+                $is_org = 0;
+                $metrics->{ $type . '_is_org_domain' } = 'no';
+                push @details, Mail::AuthenticationResults::Header::SubEntry->new()->set_key( 'policy.org_domain' )->safe_set_value( $org_domain );
             }
-        };
-        if ( my $error = $@ ) {
-            $self->handle_exception( $error );
-            $self->log_error( "ReturnOK: Domain lookup fatal error $error for $domain" );
-            push @details, Mail::AuthenticationResults::Header::SubEntry->new()->set_key( 'a.error' )->safe_set_value( 'lookup_error' );
-        }
-
-        eval {
-            $packet = $resolver->query( $domain, 'AAAA' );
-            if ($packet) {
-                foreach my $rr ( $packet->answer ) {
-                    next unless $rr->type eq "AAAA";
-                    $has_aaaa = 1;
-                    $result = 'warn';
-                    $self->{ 'metrics' }->{ 'result' } = 'warn' if $self->{ 'metrics' }->{ 'result' } ne 'pass';
-                    last;
-                }
-            }
-            else {
-                my $error = $resolver->errorstring;
-                if ( $error ) {
-                    push @details, Mail::AuthenticationResults::Header::SubEntry->new()->set_key( 'aaaa.error' )->safe_set_value( $error );
-                }
-                else {
-                    push @details, Mail::AuthenticationResults::Header::SubEntry->new()->set_key( 'aaaa.error' )->safe_set_value( 'none' );
-                }
-            }
-        };
-        if ( my $error = $@ ) {
-            $self->handle_exception( $error );
-            $self->log_error( "ReturnOK: Domain lookup fatal error $error for $domain" );
-            push @details, Mail::AuthenticationResults::Header::SubEntry->new()->set_key( 'aaaa.error' )->safe_set_value( 'lookup_error' );
-        }
-
-
-        if ( $has_a && $has_aaaa ) {
-            $self->{ 'metrics' }->{ ( $is_org ? 'org_' : '' ) . $type } = 'a_pass';
-        }
-        elsif ( $has_a ) {
-            $self->{ 'metrics' }->{ ( $is_org ? 'org_' : '' ) . $type } = 'a4_pass';
-        }
-        elsif ( $has_aaaa ) {
-            $self->{ 'metrics' }->{ ( $is_org ? 'org_' : '' ) . $type } = 'a6_pass';
+            push @details, Mail::AuthenticationResults::Header::SubEntry->new()->set_key( 'policy.is_org' )->safe_set_value( $is_org ? 'yes' : 'no' );
         }
     }
 
-    push @details, Mail::AuthenticationResults::Header::SubEntry->new()->set_key( 'result' )->safe_set_value( $result );
+    my $lookup_mx = $self->_check_domain_rr( $domain, 'MX' );
+    if ( $lookup_mx->{ 'error' } ) {
+        push @details, Mail::AuthenticationResults::Header::SubEntry->new()->set_key( 'policy.mx_error' )->safe_set_value( $lookup_mx->{ 'error' } );
+    }
 
-    my $prefix = $type . ( $is_org ? '_org' : q{} );
+    # If MX passed then that's it, stop checking
+    if ( $lookup_mx->{ 'result' } == 1 ) {
+        my $header = Mail::AuthenticationResults::Header::Entry->new()->set_key( 'x-return-mx' )->safe_set_value( 'pass' );
+        push @details, Mail::AuthenticationResults::Header::Comment->new()->safe_set_value( 'MX Record found' );
+        $metrics->{ $type . '_result' } = 'pass';
+        $metrics->{ $type . '_has_mx' } = 'yes';
+        $self->dbgout( 'ReturnOKCheck', 'pass', LOG_DEBUG );
+        foreach my $detail ( @details ) {
+            $header->add_child( $detail );
+        }
+        $self->add_auth_header($header);
+        push @{ $self->{ 'metrics' } }, $metrics;
+        return;
+    }
+    $metrics->{ $type . '_has_mx' } = 'no';
+
+    my $lookup_a    = $self->_check_domain_rr( $domain, 'A' );
+    if ( $lookup_a->{ 'error' } ) {
+        push @details, Mail::AuthenticationResults::Header::SubEntry->new()->set_key( 'policy.a_error' )->safe_set_value( $lookup_a->{ 'error' } );
+    }
+    my $lookup_aaaa = $self->_check_domain_rr( $domain, 'AAAA' );
+    if ( $lookup_aaaa->{ 'error' } ) {
+        push @details, Mail::AuthenticationResults::Header::SubEntry->new()->set_key( 'policy.aaaa_error' )->safe_set_value( $lookup_aaaa->{ 'error' } );
+    }
+
+    # If we have an A or AAAA recoed then consider this a warn.
+    if ( $lookup_a->{ 'result' } || $lookup_aaaa->{ 'result' } ) {
+        my $header = Mail::AuthenticationResults::Header::Entry->new()->set_key( 'x-return-mx' )->safe_set_value( 'warn' );
+        $metrics->{ $type . '_result' } = 'warn';
+        $self->dbgout( 'ReturnOKCheck', 'warn', LOG_DEBUG );
+        if ( $lookup_a->{ 'result' } == 1 ) {
+            $metrics->{ $type . '_has_a' } = 'yes';
+            push @details, Mail::AuthenticationResults::Header::Comment->new()->safe_set_value( 'A Record found' );
+        }
+        else {
+            $metrics->{ $type . '_has_a' } = 'no';
+        }
+        if ( $lookup_aaaa->{ 'result' } == 1 ) {
+            $metrics->{ $type . '_has_aaaa' } = 'yes';
+            push @details, Mail::AuthenticationResults::Header::Comment->new()->safe_set_value( 'AAAA Record found' );
+        }
+        else {
+            $metrics->{ $type . '_has_aaaa' } = 'no';
+        }
+        foreach my $detail ( @details ) {
+            $header->add_child( $detail );
+        }
+        $self->add_auth_header($header);
+        push @{ $self->{ 'metrics' } }, $metrics;
+        return;
+    }
+
+    if ( $is_org == 0 ) {
+        # We have DMARC to look this up, have done so, and found that we are NOT the org domain, so recheck at the org domain
+
+        my $lookup_mx = $self->_check_domain_rr( $org_domain, 'MX' );
+        if ( $lookup_mx->{ 'error' } ) {
+            push @details, Mail::AuthenticationResults::Header::SubEntry->new()->set_key( 'policy.org_mx_error' )->safe_set_value( $lookup_mx->{ 'error' } );
+        }
+
+        # If MX passed then that's it, stop checking
+        if ( $lookup_mx->{ 'result' } == 1 ) {
+            my $header = Mail::AuthenticationResults::Header::Entry->new()->set_key( 'x-return-mx' )->safe_set_value( 'warn' );
+            $self->dbgout( 'ReturnOKCheck', 'warn', LOG_DEBUG );
+            push @details, Mail::AuthenticationResults::Header::Comment->new()->safe_set_value( 'Org Domain MX Record found' );
+            foreach my $detail ( @details ) {
+            $metrics->{ $type . '_result' } = 'warn';
+            $metrics->{ $type . '_has_org_mx' } = 'yes';
+                $header->add_child( $detail );
+            }
+            $self->add_auth_header($header);
+            push @{ $self->{ 'metrics' } }, $metrics;
+            return;
+        }
+        $metrics->{ $type . '_has_org_mx' } = 'no';
+
+        my $lookup_a    = $self->_check_domain_rr( $org_domain, 'A' );
+        if ( $lookup_a->{ 'error' } ) {
+            push @details, Mail::AuthenticationResults::Header::SubEntry->new()->set_key( 'policy.org_a_error' )->safe_set_value( $lookup_a->{ 'error' } );
+        }
+        my $lookup_aaaa = $self->_check_domain_rr( $org_domain, 'AAAA' );
+        if ( $lookup_aaaa->{ 'error' } ) {
+            push @details, Mail::AuthenticationResults::Header::SubEntry->new()->set_key( 'policy.org_aaaa_error' )->safe_set_value( $lookup_aaaa->{ 'error' } );
+        }
+
+        # If we have an A or AAAA recoed then consider this a warn.
+        if ( $lookup_a->{ 'result' } || $lookup_aaaa->{ 'result' } ) {
+            my $header = Mail::AuthenticationResults::Header::Entry->new()->set_key( 'x-return-mx' )->safe_set_value( 'warn' );
+            $metrics->{ $type . '_result' } = 'warn';
+            $self->dbgout( 'ReturnOKCheck', 'warn', LOG_DEBUG );
+            if ( $lookup_a->{ 'result' } == 1 ) {
+                $metrics->{ $type . '_has_org_a' } = 'yes';
+                push @details, Mail::AuthenticationResults::Header::Comment->new()->safe_set_value( 'Org Domain A Record found' );
+            }
+            else {
+                $metrics->{ $type . '_has_org_a' } = 'no';
+            }
+            if ( $lookup_aaaa->{ 'result' } == 1 ) {
+                $metrics->{ $type . '_has_org_aaaa' } = 'yes';
+                push @details, Mail::AuthenticationResults::Header::Comment->new()->safe_set_value( 'Org Domain AAAA Record found' );
+            }
+            else {
+                $metrics->{ $type . '_has_org_aaaa' } = 'no';
+            }
+            foreach my $detail ( @details ) {
+                $header->add_child( $detail );
+            }
+            $self->add_auth_header($header);
+            push @{ $self->{ 'metrics' } }, $metrics;
+            return;
+        }
+
+    }
+
+    # We got here, we fail!
+    my $header = Mail::AuthenticationResults::Header::Entry->new()->set_key( 'x-return-mx' )->safe_set_value( 'warn' );
+    $metrics->{ $type . '_result' } = 'fail';
+    $self->dbgout( 'ReturnOKCheck', 'fail', LOG_DEBUG );
     foreach my $detail ( @details ) {
-        $detail->set_key( $prefix . '.' . $detail->key() );
-        push @{ $self->{ 'details' } }, $detail;
+        $header->add_child( $detail );
     }
+    $self->add_auth_header($header);
 
+    push @{ $self->{ 'metrics' } }, $metrics;
     return;
 }
 
 sub envfrom_callback {
     my ( $self, $env_from ) = @_;
 
-    $self->{ 'metrics' } = { 'result' => 'fail' };
-    $self->{ 'details' } = [];
+    $self->{ 'metrics' } = [];
 
     $env_from = q{} if $env_from eq '<>';
-    $self->_check_address( $env_from, 'smtp' );
+    my $addresses = $self->get_addresses_from( $env_from );
+    foreach my $address ( @$addresses ) {
+        $self->_check_address( $address, 'smtp' );
+    }
 
     return;
 }
@@ -207,7 +265,10 @@ sub header_callback {
     my ( $self, $header, $value ) = @_;
 
     if ( $header eq 'From' ) {
-        $self->_check_address( $value, 'header' );
+        my $addresses = $self->get_addresses_from( $value );
+        foreach my $address ( @$addresses ) {
+            $self->_check_address( $address, 'header' );
+        }
     }
 
     return;
@@ -216,7 +277,6 @@ sub header_callback {
 sub close_callback {
     my ( $self ) = @_;
     delete $self->{ 'metrics' };
-    delete $self->{ 'details' };
     return;
 }
 
@@ -225,15 +285,9 @@ sub eom_callback {
 
     my $metrics = $self->{ 'metrics' };
 
-    $self->dbgout( 'ReturnOKCheck', $self->{ 'metrics' }->{ 'result'} , LOG_DEBUG );
-    my $header = Mail::AuthenticationResults::Header::Entry->new()->set_key( 'x-return-mx' )->safe_set_value( $metrics->{ 'result' } );
-    foreach my $detail ( @{ $self->{ 'details' } } ) {
-        $header->add_child( $detail );
+    foreach my $metric ( @$metrics ) {
+        $self->metric_count( 'returnok_total', $metric );
     }
-
-    $self->add_auth_header($header);
-
-    $self->metric_count( 'returnok_total', $metrics );
 
     return;
 }
