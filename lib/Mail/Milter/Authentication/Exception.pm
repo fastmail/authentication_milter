719637a45d9917f428a716be362d1c27fae3ba6d
Marc Bradshaw <marc@marcbradshaw.net>
2018-06-04 14:31:58 +0200

    POD for exception object

--- lib/Mail/Milter/Authentication/Exception.pm
+++ lib/Mail/Milter/Authentication/Exception.pm
@@ -3,6 +3,14 @@ use strict;
 use warnings;
 # VERSION
 
+=constructor I<new( $args )>
+
+die Mail::Milter::Authentication::Exception->new({ 'Type' => 'Timeout', 'Text' => 'Example timeout exception' });
+
+Create a new exception object.
+
+=cut
+
 sub new {
     my ( $class, $args ) = @_;
     my $self = $args;
