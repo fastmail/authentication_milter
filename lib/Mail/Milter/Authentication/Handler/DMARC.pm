3ce929acfdb6055007de984277e81cde763da249
Marc Bradshaw <marc@marcbradshaw.net>
2019-10-31 10:58:45 +0000

    Handle errors in pre-lookup

--- lib/Mail/Milter/Authentication/Handler/DMARC.pm
+++ lib/Mail/Milter/Authentication/Handler/DMARC.pm
@@ -707,8 +707,9 @@ sub header_callback {
             $self->dbgout( 'DMARCLookup', "Anticipating $lookup", LOG_INFO );
             $resolver->bgsend( $lookup, 'TXT' );
             my $dmarc = $self->new_dmarc_object();
-            my $org_domain = eval{ $dmarc>get_organizational_domain( $domain ) };
-            if ( $org_domain && $org_domain ne $domain ) {
+            my $org_domain = eval{ $dmarc->get_organizational_domain( $domain ) };
+            $self->handle_exception( $@ );
+            if ( $org_domain && ($org_domain ne $domain) ) {
                 my $lookup = '_dmarc.'.$org_domain;
                 my $resolver = $self->get_object('resolver');
                 $self->dbgout( 'DMARCLookup', "Anticipating $lookup", LOG_INFO );
